51319c62ce8d07009b752e0dc384cff8e424453a
Shakibul Alam <shakibcste92@gmail.com>
2023-08-27 16:46:02 +0100

    total balance section with deposit done

--- deposit.js
+++ deposit.js
@@ -1,22 +1,27 @@
 //let depositValue =0;
 document.getElementById("btn-deposit").addEventListener("click",function(){
+
+    //deposit-section
     const depositInput = document.getElementById('deposit-input');
-    const newDepositAmountString = depositInput.value;
-    const newDepositAmount = parseFloat(newDepositAmountString);
+    const newDepositAmountString = depositInput.value;            //hold the value of input
+    const newDepositAmount = parseFloat(newDepositAmountString);  //convert to float
 
     depositInput.value =""; //clear input field after getting value
-
-
     
-    const depositTotalElement = document.getElementById("deposit-total");
-    const previousDepositTotalString = depositTotalElement.innerText;
-    const previousDepositTotal = parseFloat(previousDepositTotalString);
+    const depositTotalElement = document.getElementById("deposit-total");//getting id text value
+    const previousDepositTotalString = depositTotalElement.innerText;   //getting text value
+    const previousDepositTotal = parseFloat(previousDepositTotalString); //convert
 
-    const newDepositTotal = previousDepositTotal + newDepositAmount;
+    const newDepositTotal = previousDepositTotal + newDepositAmount; //calculate total deposit
     depositTotalElement.innerText = newDepositTotal;
-    
-    
 
+    //Total Balance 
+    const balanceTotalElement = document.getElementById("balance-total"); //getting text value
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotalAmount = parseFloat(previousBalanceTotalString);
 
-    //.innerText = newdepositAmount ;
+    const newBalanceTotal = previousBalanceTotalAmount + newDepositAmount;
+
+    balanceTotalElement.innerText = newBalanceTotal; //set the value in total balance area
+
 });
